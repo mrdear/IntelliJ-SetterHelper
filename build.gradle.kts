f271ca9f6be0a56964470ae4435079fc4ece5d75
屈定 <niudear@foxmail.com>
2025-04-19 06:21:13 +0800

    1. 新增25版本支持

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id("org.jetbrains.intellij") version "1.5.2"
-    java
-    idea
+    id("java")
+    id("idea")
+    id("org.jetbrains.intellij") version "1.17.4"
 }
 
 group = "cn.mrdear.setter"
-version = "0.1.17"
+version = "0.1.18"
 
 repositories {
     mavenLocal()
@@ -22,29 +22,24 @@ dependencies {
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
     // 修改为本地安装目录
-    version.set("2022.1")
-    plugins.addAll("lombok","java","Kotlin")
-    downloadSources.set(true)
+    version.set("2023.1")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf("lombok","java","Kotlin"))
+
 }
 
 
 tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
     patchPluginXml {
         // 22代表年,后面的代表季度
-        sinceBuild.set("221")
-        untilBuild.set("244.*")
-        changeNotes.set("""
-            0.1.17: update intellij 24.*
-            <br/>
-            0.1.16: update intellij 23.*
-            <br/>
-            0.1.15: add pluginIcon.svg and modify menu 'Setter Convert' to 'Model Convert'
-            <br/>
-            0.1.14: add message error pop, not error in IDE<br>
-            <em>most example in: <a href="https://github.com/mrdear/SetterHelperExample">https://github.com/mrdear/SetterHelperExample</a> </em>        """.trimIndent())
+        sinceBuild.set("231")
+        untilBuild.set("254.*")
     }
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
